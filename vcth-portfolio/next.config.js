08781d136a579144c3d117d251220bac5b33dd7b
Thu Vo <vctqs1@gmail.com>
2021-08-14 21:08:04 +0700

    remove unused code

--- vcth-portfolio/next.config.js
+++ vcth-portfolio/next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
-console.log(process.env)
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: isProd ? 'https://vctqs1.github.io/' : '',
-  images: {
-    loader: "imgix",
-    path: "https://noop/",
-  },
 }
