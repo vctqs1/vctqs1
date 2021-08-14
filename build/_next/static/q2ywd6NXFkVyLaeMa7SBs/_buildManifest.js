d31a2d4c8c19e2bb453796d2c6cffb2ceda47e93
Thu Vo <vctqs1@gmail.com>
2021-08-14 20:50:18 +0700

    restart cache

--- build/_next/static/q2ywd6NXFkVyLaeMa7SBs/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1d8f10a31d8f173e94c3.css","static/chunks/pages/index-e1bc47e2d5f0737065ec.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
