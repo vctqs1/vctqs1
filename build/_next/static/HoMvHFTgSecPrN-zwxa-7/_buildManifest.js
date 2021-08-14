b5f66ebdf1e73f710d8552555d4a14a812b5c8a6
Thu Vo <vctqs1@gmail.com>
2021-08-14 20:49:08 +0700

    add gitignore

---
+++ build/_next/static/HoMvHFTgSecPrN-zwxa-7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1d8f10a31d8f173e94c3.css","static/chunks/pages/index-cf24abf3a45cf9b0078b.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
